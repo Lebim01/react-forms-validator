4174a4ac690f702d63428e3cf340048c7589592c
Lebim01 <victoralvarezsaucedo@gmail.com>
2019-07-30 15:13:58 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ export default class Validator extends React.Component{
     }
     required( rule, value ){
         if (rule === true){
-            return value.toString().trim().length === 0;
+            return value && value.toString().trim().length === 0 ? value.toString().trim().length === 0 : false;
         }
         return false
     }
